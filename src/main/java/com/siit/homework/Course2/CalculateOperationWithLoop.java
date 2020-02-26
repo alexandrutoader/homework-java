6ac36123e92c82498b4a0657b8e07efef0d6d776
Toader Alexandru <alexandru.toader@it-genetics.ro>
2020-02-26 21:39:20 +0200

    CalculateOperationsWithLoopAndSwitch.

---
+++ src/main/java/com/siit/homework/Course2/CalculateOperationWithLoop.java
@@ -0,0 +1,42 @@
+package com.siit.homework.Course2;
+
+import java.util.Scanner;
+
+public class CalculateOperationWithLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduceti numarul de operatii de calcul pe care doriti sa le executati: ");
+        int operations = scanner.nextInt();
+
+        while (i <= operations) {
+            System.out.println("Introduceti numerele: ");
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println("Introduceti operatia: ");
+            String operation = scanner.next();
+
+            switch (operation) {
+                case "+":
+                    System.out.println("a+b= " + (a + b));
+                    break;
+                case "-":
+                    System.out.println("a-b= " + (a - b));
+                    break;
+
+                case "*":
+                    System.out.println("a*b= " + a * b);
+                    break;
+
+                case "/":
+                    System.out.println("a/b= " + (a / b));
+                    break;
+
+                default:
+                    System.out.println("Bad operation!!!");
+            }
+            i++;
+        }
+    }
+}
