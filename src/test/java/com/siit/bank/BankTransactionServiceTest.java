2a61e93e30bde43d54b65b15f97083298d709e79
Toader Alexandru <alexandru.toader@it-genetics.ro>
2020-04-04 08:58:10 +0300

    Update app tests.

--- src/test/java/com/siit/bank/BankTransactionServiceTest.java
+++ src/test/java/com/siit/bank/BankTransactionServiceTest.java
@@ -12,32 +12,52 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BankTransactionServiceTest {
     @Test
     public void given_null_input_on_add_money_when_compute_inverse_then_exception_null_is_returned() throws Exception {
-        BankAccount customerAccount = new BankAccount("ROINGB0000000");
-        customerAccount.addMoney(null);
+        try {
+            BankAccount customerAccount = new BankAccount("ROINGB0000000");
+            customerAccount.addMoney(null);
+        } catch (Throwable ex) {
+            assertThat(ex.getMessage()).isEqualTo("The provided amount is null!");
+        }
     }
 
     @Test
     public void given_minus_input_on_add_money_when_compute_inverse_then_exception_amount_greater_than_zero_is_returned() throws Exception {
-        BankAccount customerAccount = new BankAccount("ROINGB0000000");
-        customerAccount.addMoney(new BigDecimal("-100"));
+        try {
+            BankAccount customerAccount = new BankAccount("ROINGB0000000");
+            customerAccount.addMoney(new BigDecimal("-100"));
+        } catch (Throwable ex) {
+            assertThat(ex.getMessage()).isEqualTo("The amount should be greater than 0!");
+        }
     }
 
     @Test
     public void given_null_input_on_withdraw_money_when_compute_inverse_then_exception_null_is_returned() throws Exception {
-        BankAccount customerAccount = new BankAccount("ROINGB0000000");
-        customerAccount.withdrawMoney(null);
+        try {
+            BankAccount customerAccount = new BankAccount("ROINGB0000000");
+            customerAccount.withdrawMoney(null);
+        } catch (Throwable ex) {
+            assertThat(ex.getMessage()).isEqualTo("The provided amount is null!");
+        }
     }
 
     @Test
     public void given_minus_input_on_withdraw_money_when_compute_inverse_then_exception_amount_greater_than_zero_is_returned() throws Exception {
-        BankAccount customerAccount = new BankAccount("ROINGB0000000");
-        customerAccount.withdrawMoney(new BigDecimal("-100"));
+        try {
+            BankAccount customerAccount = new BankAccount("ROINGB0000000");
+            customerAccount.addMoney(new BigDecimal("-100"));
+        } catch (Throwable ex) {
+            assertThat(ex.getMessage()).isEqualTo("The amount should be greater than 0!");
+        }
     }
 
     @Test
     public void given_input_greater_than_account_ballance_when_compute_inverse_then_exception_not_enough_money_is_returned() throws Exception {
-        BankAccount customerAccount = new BankAccount("ROINGB0000000");
-        customerAccount.withdrawMoney(new BigDecimal("100"));
+        try {
+            BankAccount customerAccount = new BankAccount("ROINGB0000000");
+            customerAccount.withdrawMoney(new BigDecimal("100"));
+        } catch (Throwable ex) {
+            assertThat(ex.getMessage()).isEqualTo("Not enough money for this operation!");
+        }
     }
 
     @Test
