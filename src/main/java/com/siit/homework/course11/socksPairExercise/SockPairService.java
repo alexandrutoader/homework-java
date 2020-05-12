16a0a1e300ed0371eb312fe35cd8f35e28878890
Toader Alexandru <alexandru.toader@it-genetics.ro>
2020-05-12 18:59:07 +0300

    Added socks pair exercise.

---
+++ src/main/java/com/siit/homework/course11/socksPairExercise/SockPairService.java
@@ -0,0 +1,23 @@
+package com.siit.homework.course11.socksPairExercise;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SockPairService {
+    public int sockMerchant(int[] socks) {
+        int pairs = 0;
+        Set<Integer> socksWithoutPair = new HashSet<>();
+
+        for (int sock : socks)
+            if (!socksWithoutPair.add(sock)) {
+                pairs++;
+                socksWithoutPair.remove(sock);
+            }
+
+        socksWithoutPair.forEach(v ->
+                System.out.println("Socks without pair: " + v));
+        System.out.println("Total pairs: " + pairs);
+
+        return pairs;
+    }
+}
---
+++ src/test/java/com/siit/course11/SockPairServiceTest.java
@@ -0,0 +1,40 @@
+package com.siit.course11;
+
+import com.siit.homework.course11.socksPairExercise.SockPairService;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SockPairServiceTest {
+    private SockPairService sut;
+
+    @Before
+    public void setup() {
+        sut = new SockPairService();
+    }
+
+    @Test
+    public void given_two_pairs_input_on_array_when_compute_inverse_then_two_pairs_are_returned() {
+        int[] pairsInput = {1, 2, 1, 2};
+        int pairsNo = sut.sockMerchant(pairsInput);
+
+        assertThat(pairsNo).isEqualTo(2);
+    }
+
+    @Test
+    public void given_empty_array_input_when_compute_inverse_then_zero_pairs_is_returned() {
+        int[] pairsInput = {};
+        int pairsNo = sut.sockMerchant(pairsInput);
+
+        assertThat(pairsNo).isEqualTo(0);
+    }
+
+    @Test
+    public void given_three_pairs_input_on_array_when_compute_inverse_then_three_pairs_are_returned() {
+        int[] pairsInput = {1, 2, 1, 2, 4, 5, 2, 4};
+        int pairsNo = sut.sockMerchant(pairsInput);
+
+        assertThat(pairsNo).isEqualTo(3);
+    }
+}
